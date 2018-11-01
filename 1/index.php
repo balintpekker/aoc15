427a8eb93bc1515a9d985c55aa107cdc9ba422fa
balintpekker <balint.pekker@gmail.com>
2018-11-01 16:23:25 +0100

    Day 1

---
+++ 1/index.php
@@ -0,0 +1,21 @@
+<?php
+  $in = file_get_contents("input.txt");
+  $floor = 0;
+  $basement = false;
+  
+  for ($i = 0; $i < strlen($in); $i++) {
+    if ($in[$i] === "(") {
+	  $floor += 1;
+	} elseif ($in[$i] === ")") {
+	  $floor -= 1;
+	}
+	
+	// Day 1 Part 2
+	if ($floor === -1 && !$basement) {
+	  $basement = true;
+	  
+	  echo "Santa has entered the basement for the first time. (". ($i+1) .")<br>";
+	}
+  }
+  
+  echo "Santa is on the floor " . $floor .".";
