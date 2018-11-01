23ce06e61f49e1d706f36874a7a36009f2b04d0a
balintpekker <balint.pekker@gmail.com>
2018-11-01 16:25:03 +0100

    Day 1

--- 1/index.php
+++ 1/index.php
@@ -5,17 +5,17 @@
   
   for ($i = 0; $i < strlen($in); $i++) {
     if ($in[$i] === "(") {
-	  $floor += 1;
-	} elseif ($in[$i] === ")") {
-	  $floor -= 1;
-	}
+      $floor += 1;
+    } elseif ($in[$i] === ")") {
+      $floor -= 1;
+    }
 	
-	// Day 1 Part 2
-	if ($floor === -1 && !$basement) {
-	  $basement = true;
-	  
-	  echo "Santa has entered the basement for the first time. (". ($i+1) .")<br>";
-	}
+    // Day 1 Part 2
+    if ($floor === -1 && !$basement) {
+      $basement = true;
+        
+      echo "Santa has entered the basement for the first time. (". ($i+1) .")<br>";
+    }
   }
   
   echo "Santa is on the floor " . $floor .".";
