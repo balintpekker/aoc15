042ad3ab623b8a6a575d9dc9b01f2436cbe39be4
balintpekker <balint.pekker@gmail.com>
2018-11-01 17:06:52 +0100

    Day 2

--- 2/index.php
+++ 2/index.php
@@ -7,27 +7,27 @@
   $ribbon = 0;
   
   foreach ($presents as $present) {
-	  $dimensions = explode("x", $present);
+    $dimensions = explode("x", $present);
 	  
-	  list($length, $width, $height) = array_pad($dimensions, 3, null); //Limiting the size of the array returned by 'explode()' to 3 with 'array_pad()'. Also appending 'null' until the array contains 3 values.
+    list($length, $width, $height) = array_pad($dimensions, 3, null); //Limiting the size of the array returned by 'explode()' to 3 with 'array_pad()'. Also appending 'null' until the array contains 3 values.
+  
+    $surfaces = [
+      $length * $width,
+      $width * $height,
+      $height * $length
+    ];
 	  
-	  $surfaces = [
-	    $length * $width,
-		$width * $height,
-		$height * $length
-	  ];
+    // Day 2 Part 2
+    $extra = [
+      $length + $width,
+      $width + $height,
+      $height + $length
+    ];
 	  
-	  // Day 2 Part 2
-	  $extra = [
-		$length + $width,
-		$width + $height,
-		$height + $length
-	  ];
+    $bow = ($length * $width * $height);
 	  
-	  $bow = ($length * $width * $height);
-	  
-	  $paper += array_sum($surfaces) * 2 + min($surfaces);
-	  $ribbon += $bow + min($extra) * 2; 
+    $paper += array_sum($surfaces) * 2 + min($surfaces);
+    $ribbon += $bow + min($extra) * 2; 
   }
   
   echo "The elves should order ".$paper." total square feet of wrapping paper.<br>";
