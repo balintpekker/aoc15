42ba71750098e91b59297c6d670860838f952d4d
balintpekker <balint.pekker@gmail.com>
2018-11-01 17:51:12 +0100

    Day 3

--- 3/index.php
+++ 3/index.php
@@ -5,22 +5,22 @@
   $start = [[0,0]];
   
   for ($i = 0; $i < strlen($in); $i++) {
-	  $seq = $in[$i];
+    $seq = $in[$i];
 	  
-	  switch($seq) {
-		  case '^':
-		    $start[] = [$start[$i][0] + 1, $start[$i][1]];
-		    break;
-		  case 'v':
-		    $start[] = [$start[$i][0] - 1, $start[$i][1]];
-		    break;
-		  case '<':
-		    $start[] = [$start[$i][0], $start[$i][1] - 1];
-		    break;
-		  case '>':
-		    $start[] = [$start[$i][0], $start[$i][1] + 1];
-		    break;
-	  }
+    switch($seq) {
+      case '^':
+        $start[] = [$start[$i][0] + 1, $start[$i][1]];
+	break;
+      case 'v':
+	$start[] = [$start[$i][0] - 1, $start[$i][1]];
+	break;
+      case '<':
+	$start[] = [$start[$i][0], $start[$i][1] - 1];
+	break;
+      case '>':
+	$start[] = [$start[$i][0], $start[$i][1] + 1];
+	break;
+      }
   }
   $houses = count(array_unique($start, SORT_REGULAR));
   echo "Santa has been bringing presents to " . $houses ." houses.<br>";
@@ -30,30 +30,30 @@
   $start['robo'] = [[0,0]];
   
   for ($i = 0; $i < strlen($in); $i++) {
-	  $seq = $in[$i];
+    $seq = $in[$i];
 	  
-	  if ($i % 2 == 0) { //If remainder of $i divided by 2 is 0 then it's Santa's turn.
-	    $suffix = "santa";
-	  } else {
-		$suffix = "robo";
-	  }
+    if ($i % 2 == 0) { //If remainder of $i divided by 2 is 0 then it's Santa's turn.
+      $suffix = "santa";
+    } else {
+      $suffix = "robo";
+    }
 	  
-	  $end = end($start[$suffix]);
+    $end = end($start[$suffix]);
 	  
-	  switch($seq) {
-	    case '^':
-		  $start[$suffix][$i] = [$end[0] + 1, $end[1]];
-		  break;
-	    case 'v':
-          $start[$suffix][$i] = [$end[0] - 1, $end[1]];
-		  break;
-	    case '<':
-		  $start[$suffix][$i] = [$end[0], $end[1] - 1];
-		  break;
-	    case '>':
-		  $start[$suffix][$i] = [$end[0], $end[1] + 1];
-		  break;
-	  }
+    switch($seq) {
+      case '^':
+	$start[$suffix][$i] = [$end[0] + 1, $end[1]];
+	break;
+      case 'v':
+        $start[$suffix][$i] = [$end[0] - 1, $end[1]];
+        break;
+      case '<':
+        $start[$suffix][$i] = [$end[0], $end[1] - 1];
+	break;
+      case '>':
+        $start[$suffix][$i] = [$end[0], $end[1] + 1];
+	break;
+    }
   }
   
   echo "Santa & Robo-Santa visited ". count(array_unique($start['santa'] + $start['robo'], SORT_REGULAR)) ." houses.";
