8d8cb8252ea122162f710207b7c3adab50b3bace
balintpekker <balint.pekker@gmail.com>
2018-11-12 08:33:58 +0100

    Day 10

--- 10/index.php
+++ 10/index.php
@@ -3,7 +3,7 @@
 
   $in = "1113122113";
 
-  for ($i = 0; $i <= 40; $i++) {
+  for ($i = 0; $i < 40; $i++) {
 	  $in = preg_replace_callback('#(.)\1*#', function($x) { return strlen($x[0]).$x[0][0];}, $in);
   }
 
